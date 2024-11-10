6b7e9201eebaa44e3e1928035cbd44fa07344cc7
Elias A. Medina <medinaelias69@gmail.com>
2024-11-10 10:02:23 -0300

    update main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from website import create_app
 
 
-app = create_app('config.TestingConfig')
+# app = create_app('config.TestingConfig')
 # app = create_app('config.ProductionConfig')
-# app = create_app('config.DevelopmentConfig')
+app = create_app('config.DevelopmentConfig')
 
 
 if __name__ == '__main__':
